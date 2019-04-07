2342b4110f43e1578dd5fa1591d72592a33e096d
BakariWilkins <bxw2270@rit.edu>
2019-04-07 11:55:39 -0400

    layered imgs, continued js

--- script.js
+++ script.js
@@ -4,13 +4,49 @@
 */
 
 function setTexture(texture){
-	console.log("You chose the " + texture + "texture!");
+	var textureImg = document.getElementById("texture_img");
+	console.log(texture);
+	if(texture == "Smooth"){
+		textureImg.src = "CuseHacks/img/blank-01.png";
+	}
+	if(texture == "Craters"){
+		textureImg.src = "CuseHacks/img/terr_crater-01.png";
+	}
+	if(texture == "Clouds"){
+		textureImg.src = "CuseHacks/img/terr_cloud-01.png";
+	}
 }
 
 function setColor(color){
-	console.log("You chose the " + color + "color!");
+	var baseImg = document.getElementById("base_img");
+	console.log(color);
+	if(color == "Green"){
+		baseImg.src = "CuseHacks/img/base_green-01.png";
+	}
+	if(color == "Blue"){
+		baseImg.src = "CuseHacks/img/base_blue-01.png";
+	}
+	if(color == "Red"){
+		baseImg.src = "CuseHacks/img/base_red-01.png";
+	}
+	if(color == "Yellow"){
+		baseImg.src = "CuseHacks/img/base_yellow-01.png";
+	}
 }
 
+function setAddOn(addOn) {
+	var addOnImg = document.getElementById("add_on_img");
+	console.log(addOn);
+	if (addOn == "Moon") {
+		addOnImg.src = "CuseHacks/img/add_moon-01.png";
+	}
+	if (addOn == "Ring") {
+		addOnImg.src = "CuseHacks/img/add_ring-01.png";
+	}
+	if (addOn == "Ship") {
+		addOnImg.src = "CuseHacks/img/add_ship-01.png";
+	}
+}
 
 function myFunction(){
 	//var texture = document.getElementById("texture").value;
