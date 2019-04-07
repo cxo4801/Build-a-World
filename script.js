ddde16d34558c0df90756f4c519d06144573d2fe
cxo4801 <cxo4801@g.rit.edu>
2019-04-06 21:25:25 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+/* Carolyn Opre
+	April 6th & 7th 2019
+	CuseHack
+*/
+
+function getTexture(){
+	var texture = document.getElementById("texture").value;
+	alert("You chose the " + texture + "texture!";
+}
+
+
+function getContent(){
+	var texture = document.getElementById("texture").value;
+	var texture = document.getElementById("color").value;
+	var content = ;
+	
+	window.open('mailto:cxo4801@rit.edu?subject=Your Brand New World&body='+content);
+}
+
+//----------------------------------------------------------------------------------------
+function getFormValues() {
+// function to send first and last names
+// to an 'alert' message.
+	var f = document.getElementById("fname").value;
+	var l = document.getElementById("lname").value;
+	alert(f + " " + l);
+}
